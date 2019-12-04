9bd68ab3d135eb4ee316f0bdddb03a9b9ce466c0
MatthewKing98 <matthewsky98@gmail.com>
2019-12-04 03:06:46 -0500

    Added functions and desc. renamed due to js lang

---
+++ project_sia/src/adminCommandsManager.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class adminCommandsManager{
+    /*
+    Allows user to add event to official event list. Reserved for advisors or
+    department staff capable of verifying/hosting such events.
+    */
+    updateEventsList() {
+        
+    }
+}
+export default adminCommandsManager;
