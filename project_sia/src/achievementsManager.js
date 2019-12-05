5e0529b36e12d915bd8e387169f29738c3999fed
André Vincent <andrevncnt7@gmail.com>
2019-12-04 23:59:07 -0500

    Adds achievements and utility files

---
+++ project_sia/src/achievementsManager.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class AchievementsManager {
+	/*
+	Responsible for updating Achievements associated with a user.
+
+	*/
+	
+	fetchUserAchievements() {
+
+	}
+
+	saveAchievements() {
+
+	}
+
+	checkAchievements() {
+		/* interrupt based */
+	}
+
+	checkAchievements() {
+		/* poll based */
+	}
+
+}
---
+++ project_sia/src/utilityTools.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class utilityTools {
+	/*
+		Responsible for receiving notifications about user data
+		user achievements, and events.
+	*/
+	
+	pushNotifications() {
+
+	}
+
+
+}
