1dd5ca9a5ffcc9f6e4075844871ddea303b0fdce
André Vincent <andrevncnt7@gmail.com>
2020-02-27 14:08:20 -0500

    Expo push notification tool work

--- sia-react-native/screens/Home.js
+++ sia-react-native/screens/Home.js
@@ -7,11 +7,13 @@ import {
   Button
 } from 'react-native'
 import {
-  connect
+  connect, Keyboard
 } from 'react-redux'
 import Firebase from '../Firebase'
-import {Notifications} from 'expo'
+import {Notifications, Constants} from 'expo'
 import * as Permissions from 'expo-permissions'
+import {LocalNotification,PushNotification} from '../notification/Notification'
+
 
 class Home extends React.Component {
   handleSignout = () => {
@@ -24,19 +26,20 @@ class Home extends React.Component {
     // iOS won't necessarily prompt the user a second time.
     // On Android, permissions are granted on app installation, so
     // `askAsync` will never prompt the user
-  
     // Stop here if the user did not grant permissions
+
     if (status !== 'granted') {
-      alert('No notification permissions!')
-      return
+      alert("Permissions not granted")
     }
+
     try {
+
     // Get the token that identifies this device
     let token = await Notifications.getExpoPushTokenAsync()
-    console.log("Here, dummy!" + token)
+    console.log("Token: " + token)
   
     // POST the token to your backend server from where you can retrieve it to send push notifications.
-    Firebase.database().ref('users/'+this.currentUser.uid+'/push_token').set(token)
+    // Firebase.database().ref('users/'+this.currentUser.uid+'/push_token').set(token)
     }
     catch(error) {
       console.log(error)
@@ -47,7 +50,9 @@ class Home extends React.Component {
     await this.registerForPushNotificationsAsync()
     this.currentUser = Firebase.auth().currentUser
   }
-
+// TODO: Pass notification object from notification file here,
+// and create an object to be presented as a local notification. 
+// Should at least have an ID
   sendPushNotification = () => {
     let response = fetch('https://exp.host/--/api/v2/push/send', {
       method: 'POST',
@@ -59,6 +64,8 @@ class Home extends React.Component {
       // TODO: must set the token/recipient of notification to be self, not another device
       // TODO: token must also be saved with the record of signing up
       body: {
+
+        // TODO: Should be able to store Token instead of hard-coding it
         to: 'ExponentPushToken[Htwd8-Oq4-3FmCyoH8OeeT]',
         sound: 'default',
         title: 'Demo',
@@ -71,7 +78,7 @@ class Home extends React.Component {
       <Text> Profile Screen </Text> 
       <Text> {this.props.user.email} </Text> 
       <Button title='Logout' onPress={this.handleSignout}/> 
-      <Button title='Send Push Notification' onPress={() => this.sendPushNotification()}/>
+      <Button title='Send Push Notification' onPress={() => {this.scheduleNotif()}}/>
       </View>
     )
   }
