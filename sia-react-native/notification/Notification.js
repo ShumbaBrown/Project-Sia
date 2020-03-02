382e75c858d6accc42a5f227438517dee882df40
André Vincent <andrevncnt7@gmail.com>
2020-03-01 22:47:35 -0500

    initial setup

---
+++ sia-react-native/notification/Notification.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// LocalNotification is responsible for creating notifications
+// that appear inside the application/while the application is active.
+class LocalNotification extends React.Component {
+    constructor() {
+        let items = {
+            title: '',
+            body: '',
+            }
+        }
+        tags = []
+}
+// Separate into one central class that is the parent
+// the two subclasses, local and push notifications **if there is
+// something that differentiates the two. If not just use one.
+export default {LocalNotification}
