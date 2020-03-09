86908a28868974b3de498a0d3c27bddd25700db0
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2020-03-08 20:04:26 -0400

    Adding styling
    Adding some styling to the login screen

--- sia-react-native/screens/Login.js
+++ sia-react-native/screens/Login.js
@@ -5,6 +5,8 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
   Text,
   Button
 } from 'react-native'
@@ -14,6 +16,12 @@ import { updateEmail, updatePassword, login } from '../actions/user'
 import Backend from '../Backend.js'
 import event from '../classes/event.js'
 import user from '../classes/user.js'
+
+const DismissKeyboard = ({ children }) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 class Login extends React.Component {
   state = {
     email: "",
@@ -28,29 +36,42 @@ class Login extends React.Component {
 
   render() {
         return (
+          <DismissKeyboard>
             <View style={styles.container}>
+            <Text style={styles.logo}>SIA</Text>
+            <View style={styles.inputView} >
                 <TextInput
-                    style={styles.inputBox}
+                    style={styles.inputText}
                     value={this.props.user.email}
                     onChangeText={email => this.props.updateEmail(email)}
                     placeholder='Email'
+                    placeholderTextColor="#003f5c"
                     autoCapitalize='none'
                 />
+            </View>
+            <View style={styles.inputView} >
                 <TextInput
-                    style={styles.inputBox}
+                    style={styles.inputText}
                     value={this.props.user.password}
                     onChangeText={password => this.props.updatePassword(password)}
                     placeholder='Password'
+                    placeholderTextColor="#003f5c"
                     secureTextEntry={true}
                 />
-                <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
-                    <Text style={styles.buttonText}>Login</Text>
+            </View>
+                <TouchableOpacity>
+                  <Text style={styles.forgot}>Forgot Password?</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.loginBtn} onPress={this.handleLogin}>
+                    <Text style={styles.loginText}>Login</Text>
                 </TouchableOpacity>
                 <Button
                     title="Don't have an account yet? Sign up"
                     onPress={() => this.props.navigation.navigate('Signup')}
                 />
             </View>
+          </DismissKeyboard>
         )
     }
 }
@@ -58,18 +79,28 @@ class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#003f5c',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  inputBox: {
-    width: '85%',
-    margin: 10,
-    padding: 15,
-    fontSize: 16,
-    borderColor: '#d3d3d3',
-    borderBottomWidth: 1,
-    textAlign: 'center'
+  inputView: {
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20
+  },
+  inputText: {
+    height: 50,
+    color: "white"
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40
   },
   button: {
     marginTop: 30,
@@ -82,13 +113,28 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: 200
   },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
   buttonText: {
     fontSize: 20,
     fontWeight: 'bold',
     color: '#fff'
   },
-  buttonSignup: {
-    fontSize: 12
+  forgot: {
+    marginTop: 30,
+    color: "white",
+    fontSize: 11
+  },
+  loginText: {
+    color: "white"
   }
 })
 
