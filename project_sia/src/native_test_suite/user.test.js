68e8b809b59bc039e45012fef29c96ad5cba3fae
MatthewKing98 <matthewsky98@gmail.com>
2020-03-09 19:35:36 -0400

    Removed user helper functions. Added achievements

--- project_sia/src/native_test_suite/user.test.js
+++ project_sia/src/native_test_suite/user.test.js
@@ -1,4 +1,7 @@
 import user from '../../../sia-react-native/classes/user';
+import statistic from '../../../sia-react-native/classes/statistic';
+import statisticList from '../../../sia-react-native/classes/statisticList.js';
+import achievement from '../../../sia-react-native/classes/achievement';
 
 describe("User initialization tests",() => {
     it("declares a default object successfully", () => {
@@ -13,13 +16,55 @@ describe("User initialization tests",() => {
             gender:"UNSET",
             classification:"UNSET",
             major:"UNSET",
-            interest_tags: new Set()
+            interest_tags: new Set(),
+            statLibrary: new statisticList(),
+            achievements: []
         };
 
         expect(generated_user).toEqual(expected_user);
     });
     
     it("declares an object based on passed parameters", () => {
+        const statistic_a = new statistic(
+            1,
+            "Sample Stat A",
+            "Given Description A",
+            false,
+            false,
+            3
+            );
+        const statistic_b = new statistic(
+            2,
+            "Sample Stat B",
+            "Given Description B",
+            true,
+            true,
+            0
+            );
+
+        const expected_achievement = {
+            id: 123,
+            name: 'Sample',
+            description: 'Sample desc',
+            state_requirements: [
+                {
+                    id: 256,
+                    name: "Sample Stat A",
+                    description: "Given Description A",
+                    isBoolean: false,
+                    flag: false,
+                    quantity: 1
+                },
+                {
+                    id: 985,
+                    name: "Sample Stat B",
+                    description: "Given Description B",
+                    isBoolean: true,
+                    flag: true,
+                    quantity: 0
+                }
+            ]
+        };
 
         const generated_user = new user(
             53,
@@ -30,7 +75,9 @@ describe("User initialization tests",() => {
             "Male",
             "Senior",
             "Computer Science",
-            new Set(["HobbyA","HobbyB"])
+            new Set(["HobbyA","HobbyB"]),
+            new statisticList([statistic_a,statistic_b]),
+            [expected_achievement]
             );
 
     
@@ -43,106 +90,14 @@ describe("User initialization tests",() => {
             gender:"Male",
             classification:"Senior",
             major:"Computer Science",
-            interest_tags:new Set(["HobbyA","HobbyB"])
+            interest_tags: new Set(["HobbyA","HobbyB"]),
+            statLibrary: new statisticList([statistic_a,statistic_b]),
+            achievements: [expected_achievement]
         };
     
         expect(generated_user).toEqual(expected_user);
     });
     
-});
-
-describe("User getter tests",() => {
-    const generated_user = new user(
-        53,
-        "Bob",
-        "Johnson",
-        "bob.john@sample.com",
-        34,
-        "Male",
-        "Senior",
-        "Computer Science",
-        new Set(["HobbyA","HobbyB"])
-        );
-
-    it("gets ID", () => {
-        expect(generated_user.getID()).toEqual(53);
-    });
-
-    it("gets first name", () => {
-        expect(generated_user.getFirstName()).toEqual("Bob");
-    });
-
-    it("gets last name", () => {
-        expect(generated_user.getLastName()).toEqual("Johnson");
-    });
-
-    it("gets email address", () => {
-        expect(generated_user.getEmail()).toEqual("bob.john@sample.com");
-    });
-
-    it("gets age", () => {
-        expect(generated_user.getAge()).toEqual(34);
-    });
-
-    it("gets gender", () => {
-        expect(generated_user.getGender()).toEqual("Male");
-    });
-
-    it("gets classification", () => {
-        expect(generated_user.getClassification()).toEqual("Senior");
-    });
-
-    it("gets major", () => {
-        expect(generated_user.getMajor()).toEqual("Computer Science");
-    });
-
-    it("gets interest tags", () => {
-        expect(generated_user.getInterestTags()).toEqual(new Set(["HobbyA","HobbyB"]));
-    });
-});
-
-describe("User setter tests",() => {
-    const generated_user = new user();
-    
-    it("sets ID", () => {
-        generated_user.setID(53);
-        expect(generated_user.getID()).toEqual(53);
-    });
-
-    it("sets first name", () => {
-        generated_user.setFirstName("SampleFName");
-        expect(generated_user.getFirstName()).toEqual("SampleFName");
-    });
-
-    it("sets last name", () => {
-        generated_user.setLastName("SampleLName");
-        expect(generated_user.getLastName()).toEqual("SampleLName");
-    });
-
-    it("sets email address", () => {
-        generated_user.setEmail("sample@sample.com")
-        expect(generated_user.getEmail()).toEqual("sample@sample.com");
-    });
-
-    it("sets age", () => {
-        generated_user.setAge(17)
-        expect(generated_user.getAge()).toEqual(17);
-    });
-
-    it("sets gender", () => {
-        generated_user.setGender("Female")
-        expect(generated_user.getGender()).toEqual("Female");
-    });
-
-    it("sets classification", () => {
-        generated_user.setClassification("Junior")
-        expect(generated_user.getClassification()).toEqual("Junior");
-    });
-
-    it("sets major", () => {
-        generated_user.setMajor("Mechanical Engineering")
-        expect(generated_user.getMajor()).toEqual("Mechanical Engineering");
-    });    
 });
 
 describe("User interest tags tests",() => {
@@ -172,5 +127,167 @@ describe("User interest tags tests",() => {
         generated_user.deleteInterestTag("SampleC");
         expect(generated_user.getInterestTags()).toEqual(new Set(["SampleA"]));
     });
+});
+
+describe("User achievements tests",() => {
+    const sample_user_state = new statisticList(
+        [
+            {
+                id: 1,
+                name: "Backup Email Set",
+                description: "Tracks if user has a backup email",
+                isBoolean: true,
+                flag: true,
+                quantity: 0
+            },
+            {
+                id: 2,
+                name: "Profile Picture Changed",
+                description: "Tracks if user changed their profile picture",
+                isBoolean: true,
+                flag: true,
+                quantity: 0
+            },
+            {
+                id: 3,
+                name: "Events Made",
+                description: "Tracks how many events are created",
+                isBoolean: false,
+                flag: false,
+                quantity: 1
+            },
+            {
+                id: 4,
+                name: "Clicks Made",
+                description: "Tracks Number of clicks in the app",
+                isBoolean: false,
+                flag: false,
+                quantity: 899
+            }
+        ]
+    )
+
+    const sample_achievement_a = new achievement(
+        1,
+        "Got Your Back",
+        "Awarded for setting a backup email.",
+        new statisticList(
+            [
+                new statistic(
+                    1,
+                    "Backup Email Set",
+                    "Tracks if user has a backup email",
+                    true,
+                    true,
+                    0
+                )
+        ])
+    );
     
+    const sample_achievement_b = new achievement(
+        2,
+        "Looking Fabulous",
+        "Awarded for changing your profile pic from default",
+        new statisticList(
+            [
+                new statistic(
+                    2,
+                    "Profile Picture Changed",
+                    "Tracks if user changed their profile picture",
+                    true,
+                    true,
+                    0
+                )
+        ])
+    );
+    
+    
+    const sample_achievement_c = new achievement(
+        3,
+        "Busy Body",
+        "Awarded for making at least 5 events",
+        new statisticList(
+            [
+                new statistic(
+                    3,
+                    "Events Made",
+                    "Tracks how many events are created",
+                    false,
+                    false,
+                    5
+                )
+        ])
+    );
+
+    it("successfully generates an achievements list", () => {
+        let achievements_library = [sample_achievement_a,sample_achievement_b,sample_achievement_c];
+
+        const sample_user = new user(
+            53,
+            "Bob",
+            "Johnson",
+            "bob.john@sample.com",
+            34,
+            "Male",
+            "Senior",
+            "Computer Science",
+            new Set(["HobbyA","HobbyB"]),
+            sample_user_state,
+            []
+            );
+
+        const expected_user = new user(
+            53,
+            "Bob",
+            "Johnson",
+            "bob.john@sample.com",
+            34,
+            "Male",
+            "Senior",
+            "Computer Science",
+            new Set(["HobbyA","HobbyB"]),
+            sample_user_state,
+            [sample_achievement_a,sample_achievement_b]
+            );
+
+        sample_user.updateAchievements(achievements_library);
+
+        expect(sample_user).toEqual(expected_user);
+    });
+
+    it("sucessfully updates an achievement list", () => {
+        let achievements_library = [sample_achievement_a,sample_achievement_b,sample_achievement_c];
+
+        const sample_user = new user(
+            53,
+            "Bob",
+            "Johnson",
+            "bob.john@sample.com",
+            34,
+            "Male",
+            "Senior",
+            "Computer Science",
+            new Set(["HobbyA","HobbyB"]),
+            sample_user_state,
+            [sample_achievement_c]
+            );
+
+        const expected_user = new user(
+            53,
+            "Bob",
+            "Johnson",
+            "bob.john@sample.com",
+            34,
+            "Male",
+            "Senior",
+            "Computer Science",
+            new Set(["HobbyA","HobbyB"]),
+            sample_user_state,
+            [sample_achievement_a,sample_achievement_b]
+            );
+
+        sample_user.updateAchievements(achievements_library);
+
+        expect(sample_user).toEqual(expected_user);
+    });
 });
