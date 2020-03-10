25d9ad515fba9e17b7a953c129277b3d1f6b157a
André Vincent <andrevncnt7@gmail.com>
2020-03-10 12:54:57 -0400

    reformats Notification class to a react component and refactors notification into its own branch

--- project_sia/src/native_test_suite/notification.test.js
+++ project_sia/src/native_test_suite/notification.test.js
@@ -1,16 +1,21 @@
-import Notification from '../../../sia-react-native/classes/notification'
+import Notification from '../../../sia-react-native/notification/notification'
 import { interval } from 'rxjs'
 
 describe("Notification initialization", () => {
     it("creates a new notification object", () => {
         const newNotification = new Notification()
-
         const expectNotification = {
-            state : {willDisplay: false, tags: []},
+            state : {willDisplay: false},
             title : 'Notification Title',
             message : 'Notification Message',
+        
             type : 'UNSET'
         }
-        expect(newNotification).toEqual(expectNotification)
+        expect(newNotification.state).toEqual(expectNotification.state)
+        expect(newNotification.title).toEqual(expectNotification.title)
+        expect(newNotification.message).toEqual(expectNotification.message)
+        expect(newNotification.type).toEqual(expectNotification.type)
     })
 })
+
+// TODO: More notifications will come as triggers are decided
