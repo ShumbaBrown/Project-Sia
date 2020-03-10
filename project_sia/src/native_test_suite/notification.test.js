7d4c72dab51d1326ff7537a3c643affa2530837d
André Vincent <andrevncnt7@gmail.com>
2020-03-10 10:36:36 -0400

    Adds edits to notification class and begins test

--- project_sia/src/native_test_suite/notification.test.js
+++ project_sia/src/native_test_suite/notification.test.js
@@ -1,16 +1,16 @@
 import Notification from '../../../sia-react-native/classes/notification'
 import { interval } from 'rxjs'
 
-describe("Notification initialization"), () => {
-    it("creates a new notification object"), () => {
+describe("Notification initialization", () => {
+    it("creates a new notification object", () => {
         const newNotification = new Notification()
 
         const expectNotification = {
-            state = {willDisplay: false, tags: []},
-            title = 'Notification Title',
-            message = 'Notification Message',
+            state : {willDisplay: false, tags: []},
+            title : 'Notification Title',
+            message : 'Notification Message',
+            type : 'UNSET'
         }
-
         expect(newNotification).toEqual(expectNotification)
-    }
-}
+    })
+})
