0eb5f1f320279ec7cc7a24a86088802d32297c89
MatthewKing98 <matthewsky98@gmail.com>
2020-03-11 18:36:45 -0400

    Added tags filter. Added tests. Fixed old tests

--- sia-react-native/classes/eventList.js
+++ sia-react-native/classes/eventList.js
@@ -55,5 +55,39 @@ class eventList {
             return [new event(), false];
         }
     }
+
+    //generates ordered list of events containing the passed tags
+    filterEventListByTags(filter_tags = []) {
+        let filtered_events = [];
+        let final_filtered_list = [];
+
+        for(let event_cycler = 0; event_cycler < this.events.length; event_cycler++)
+        {
+            let tags_matched = 0;
+            for(let tag_cycler = 0; tag_cycler < filter_tags.length; tag_cycler++)
+            {
+                if(this.events[event_cycler].tags.includes(filter_tags[tag_cycler])) {
+                    tags_matched += 1;
+                }
+            }
+
+            if(tags_matched > 0) {
+                filtered_events.push([tags_matched,this.events[event_cycler]]);
+            }
+        }
+
+        //sorts by most relevant
+        filtered_events.sort(function(first_ele, second_ele) {
+            return second_ele[0] - first_ele[0];
+        });
+
+        //strips numbering
+        for(let event_cycler = 0; event_cycler < filtered_events.length; event_cycler++)
+        {
+            filtered_events[event_cycler] = filtered_events[event_cycler][1];
+        }
+
+        return filtered_events;
+    }
 }
 export default eventList;
