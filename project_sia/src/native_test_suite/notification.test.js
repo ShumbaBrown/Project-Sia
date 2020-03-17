95982839e64242b26effec2e8ae5fa91307732fe
André Vincent <andrevncnt7@gmail.com>
2020-03-17 16:46:18 -0500

    adds one more test to testing suite

--- project_sia/src/native_test_suite/notification.test.js
+++ project_sia/src/native_test_suite/notification.test.js
@@ -1,20 +1,30 @@
-import Notification from '../../../sia-react-native/notification/notification'
+import Notification from '../../../sia-react-native/classes/notification'
+import ReactDOM from 'react-dom'
+import React from 'react'
 import { interval } from 'rxjs'
 
 describe("Notification initialization", () => {
+    const newNotification = new Notification()
+    beforeEach(function() {
+        spyOn(newNotification, 'toggleDisplay')
+    })
     it("creates a new notification object", () => {
-        const newNotification = new Notification()
+        
         const expectNotification = {
             state : {willDisplay: false},
             title : 'Notification Title',
             message : 'Notification Message',
-        
             type : 'UNSET'
         }
         expect(newNotification.state).toEqual(expectNotification.state)
         expect(newNotification.title).toEqual(expectNotification.title)
         expect(newNotification.message).toEqual(expectNotification.message)
         expect(newNotification.type).toEqual(expectNotification.type)
+    })
+
+    it("changes display when toggled", () => {
+        newNotification.toggleDisplay()
+        expect(newNotification.toggleDisplay).toHaveBeenCalled()
     })
 })
 
