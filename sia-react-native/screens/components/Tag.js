85831c83a348539cb5de4a9ec4ed5ccf007b05d8
Tariq Cranston <32856620+TariqC7@users.noreply.github.com>
2020-04-25 02:25:39 -0400

    Create Tag.js

---
+++ sia-react-native/screens/components/Tag.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet
+} from "react-native";
+
+class Tag extends Component {
+    render() {
+        return (
+            <View style={{
+                minHeight: 20, minWidth: 40, padding: 5, backgroundColor: 'white', backgroundColor: '#1a2733', borderColor: '#1a2733', borderWidth: 1, borderRadius: 2, marginRight: 5
+            }}>
+                <Text style={{ fontWeight: '700', color: 'white', fontSize: 10 }}>{this.props.name}</Text>
+            </View>
+        );
+    }
+}
+export default Tag;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
